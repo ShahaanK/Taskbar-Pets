8f0581f66349378cbd60d6bff0eabfec208a1845
Shahaan Khan <shahaanzia1@gmail.com>
2022-11-09 01:03:31 -0500

    Made the UI stay instead of disappearing instantly

--- Game.py
+++ Game.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame
 
 if __name__ == "__main__":
@@ -6,3 +8,11 @@ if __name__ == "__main__":
     size = (800, 600)
     screen = pygame.display.set_mode(size)  # Sets the size of our screen.
     pygame.display.set_caption("Taskbar Pets")
+
+    # --- Main event loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+        pygame.draw.rect(screen, (0, 111, 111), (50,50,100,100), 0)
+        pygame.display.flip()
