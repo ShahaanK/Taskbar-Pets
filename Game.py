c7066819593ffe93de027bdfe6cfc5760258577f
Shahaan Khan <shahaanzia1@gmail.com>
2022-11-09 00:39:47 -0500

    Adding Pygame to game, this is the setup

---
+++ Game.py
@@ -0,0 +1,8 @@
+import pygame
+
+if __name__ == "__main__":
+    pygame.init()
+
+    size = (800, 600)
+    screen = pygame.display.set_mode(size)  # Sets the size of our screen.
+    pygame.display.set_caption("Taskbar Pets")
