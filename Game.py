56a15fd21ecaa14a1e2225510098db551f25fb3e
Shahaan Khan <shahaanzia1@gmail.com>
2022-11-15 00:02:16 -0500

    Made the Health, hunger and mood. Need to further tweek. Made it display.

--- Game.py
+++ Game.py
@@ -3,19 +3,35 @@ import pygame
 # Calling the classes.
 from Pets import Pets
 
+
+def keyCheck():
+    # If you press space bar the dogs hunger will go up.
+    if event.key == pygame.K_SPACE:
+        doggo.feed()
+
+    if event.key == pygame.K_p:
+        doggo.play()
+
+    if event.key == pygame.K_r:
+        doggo.sleep()
+
+
 if __name__ == "__main__": # Initializer of our game so we load here.
     pygame.init()
 
+    # UI stuff.
     size = (800, 600)
     screen = pygame.display.set_mode(size)  # Sets the size of our screen.
     pygame.display.set_caption("Taskbar Pets")
+    myfont = pygame.font.Font('freesansbold.ttf', 30) # Basic font.
 
-    doggoImage = pygame.image.load("Pics/doggo.JPG"); # Setting the image.
+    # Setting the images.
+    doggoImage = pygame.image.load("Pics/doggo.JPG");
 
-    doggo = Pets(300, 200, doggoImage) # Initializing the first pet.
+    # Initializing the first pets.
+    doggo = Pets(300, 200, doggoImage)
 
     done = False # Loop until the user clicks the close button. At end.
-
 
     # --- Main Program loop
     while not done:
@@ -23,11 +39,28 @@ if __name__ == "__main__": # Initializer of our game so we load here.
         for event in pygame.event.get(): # User does something.
             if event.type == pygame.QUIT: # If they click close.
                 done = True # Change the flag so we are done
+            elif event.type == pygame.KEYDOWN: # User preses a key. FIXME: Change later to be mouse button thingy
+                keyCheck()
 
+        # Drawing stuff to the screen ------------------------------------------------------
         screen.fill(0)  # Background color.
-        pygame.draw.rect(screen, (0, 111, 111), (50,50,100,100), 0)
+        pygame.draw.rect(screen, (0, 111, 111), (50, 50, 100, 100), 0)
 
         doggo.draw(screen) # Uses the method to draw it to screen.
 
+        # Print out the hunger.
+        hunger_text = myfont.render("Hunger: " + str(doggo.hunger), 1, (0, 255, 124)) # Setting text.
+        screen.blit(hunger_text, (20, 20)) # Printing text to screen.
+
+        # Print out the Health.
+        hunger_text = myfont.render("Health: " + str(doggo.health), 1, (0, 255, 124))  # Setting text.
+        screen.blit(hunger_text, (40, 40))  # Printing text to screen.
+
+        # Print out the Mood.
+        hunger_text = myfont.render("Mood: " + str(doggo.mood), 1, (0, 255, 124))  # Setting text.
+        screen.blit(hunger_text, (60, 60))  # Printing text to screen.
 
         pygame.display.flip() # Updates the game screen so keep on bottom.
+
+
+
--- Pets.py
+++ Pets.py
@@ -4,13 +4,30 @@ import pygame
 class Pets():
 
     def __init__(self, x, y, img):
-        self.xPos = x;
-        self.yPos = y;
-        self.imgOfPet = img;
-        self.hunger = 100; # Hunger meter.
-        self.state = True; # Alive or dead.
-        self.age = 0; # Age of pet.
+        self.xPos = x
+        self.yPos = y
+        self.imgOfPet = img
+        self.hunger = 100 # Hunger meter.
+        self.health = 100 # Health bar.
+        self.mood = 100 # Mood bar.
+        self.rest = False # If the pet is sleeping.
+        self.state = True # Alive or dead.
+        self.age = 0 # Age of pet.
 
     def draw(self, screen):
         screen.blit(self.imgOfPet, (self.xPos, self.yPos))
 
+    def feed(self): # Feed the pet to increase its hunger, mood, health.
+        self.hunger += 5
+        self.health += 2
+        self.mood += 2
+
+    def play(self): # Play with the pet to decrease the hunger and health but increase mood.
+        self.hunger -= 3
+        self.health -= 5
+        self.mood += 5
+
+    def sleep(self): # If the pet sleeps, recover health but decrease hunger and mood.
+        self.health += 10
+        self.hunger -= 3
+        self.mood -= 1
