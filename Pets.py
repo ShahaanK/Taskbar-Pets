bd88cb587f5a07d3686c949574737bf7f258e4ec
Shahaan Khan <shahaanzia1@gmail.com>
2022-11-14 22:38:42 -0500

    Creating a backend for this project with base features needed for pets

--- Pets.py
+++ Pets.py
@@ -1,3 +1,13 @@
 import pygame
 
 
+class Pets():
+
+    def __init__(self):
+        self.xPos = 0;
+        self.yPos = 0;
+        self.imgOfPet;
+        self.hunger = 100; # Hunger meter.
+        self.state = True; # Alive or dead.
+        self.age = 0; # Age of pet.
+
